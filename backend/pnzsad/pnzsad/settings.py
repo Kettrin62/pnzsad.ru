23dde4fdc3d26a5e1804245350f439ebb9da361d
Hash466 <80330603+Hash466@users.noreply.github.com>
2022-03-15 17:16:07 +0300

    Update settings.py

--- backend/pnzsad/pnzsad/settings.py
+++ backend/pnzsad/pnzsad/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
-        'POSTGRES_USER': os.environ.get('POSTGRES_USER'),
+        'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
